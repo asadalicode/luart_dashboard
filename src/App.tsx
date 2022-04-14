26d1d21e48a3da5c51045f07e249a26e2d2f89f5
Asad Ali <asadali.code123@gmail.com>
2022-04-14 23:41:13 +0500

    sidebar init

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './modules/shared/components/navbar/navbar';
 import Footer from './modules/shared/components/footer/footer';
 import { useState } from 'react';
+import SideNavbar from './modules/shared/components/sideNavbar/sideNavbar';
+import { Style } from '@mui/icons-material';
 
 function App() {
   const [isMobileNavbarOpen, setIsMobileNavbarOpen] = useState(false);
@@ -16,7 +18,17 @@ function App() {
       <Navbar handleMobileNavbar={handleMobileNavbar} />
       {
         !isMobileNavbarOpen &&
-        <Footer />
+        <>
+          <div className="container">
+
+          <div className="d-flex">
+            <div className="sideBar">
+              <SideNavbar />
+            </div>
+          </div>
+          </div>
+          <Footer />
+        </>
       }
     </div>
   );
---
+++ src/modules/shared/components/sideNavbar/sideNavbar.tsx
@@ -0,0 +1,60 @@
+import Style from './sideNavbar.module.scss';
+import { ReactComponent as DashboardIcon } from '../../../../assets/images/dashboard.svg';
+import { ReactComponent as TradeIcon } from '../../../../assets/images/trade.svg';
+import { ReactComponent as StakingIcon } from '../../../../assets/images/staking.svg';
+import { ReactComponent as Liquiditycon } from '../../../../assets/images/liquidity.svg';
+import { ReactComponent as VestingIcon } from '../../../../assets/images/vesting.svg';
+import { ReactComponent as LeaderboardIcon } from '../../../../assets/images/leaderboard.svg';
+import { ReactComponent as MarketplaceIcon } from '../../../../assets/images/marketplace.svg';
+import { ReactComponent as LaunchpadIcon } from '../../../../assets/images/launchpad.svg';
+
+const pages = ['Launchpad', 'Marketplace'];
+const sideBarPages = [
+    { Icon: DashboardIcon, title: 'Dashboard', acitveItem: false },
+    { Icon: TradeIcon, title: 'Trade' , acitveItem: false },
+    { Icon: StakingIcon, title: 'Staking', acitveItem: true  },
+    { Icon: Liquiditycon, title: 'Liquidity', acitveItem: false  },
+    { Icon: VestingIcon, title: 'Vesting', acitveItem: false  }
+]
+const sideBarPages1 = [
+    { Icon: LeaderboardIcon, title: 'Leaderboard', acitveItem: false  },
+    { Icon: MarketplaceIcon, title: 'Marketplace', acitveItem: false  },
+    { Icon: LaunchpadIcon, title: 'Launchpad', acitveItem: false  }
+]
+
+const SideNavbar = () => {
+    return (
+        <div className={Style.sideNavbar}>
+            <div className={Style.container}>
+                <div>
+
+                    <h6 className={Style.title}>LUART Application</h6>
+                    {sideBarPages.map((item) => {
+                        return (
+                            <div>
+                                <a className={`d-flex align-items-center ${Style.item} ${item.acitveItem ? Style.activeItem : ''}`}>
+                                    <item.Icon height={20} /> <span className='ms-2'>{item.title}</span>
+                                </a>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className='mt-5'>
+
+                    <h6 className={Style.title}>Others</h6>
+                    {sideBarPages1.map((item) => {
+                        return (
+                            <div>
+                                <a className={`d-flex align-items-center ${Style.item}`}>
+                                    <item.Icon height={20} /> <span className='ms-2'>{item.title}</span>
+                                </a>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+
+    );
+}
+export default SideNavbar;
